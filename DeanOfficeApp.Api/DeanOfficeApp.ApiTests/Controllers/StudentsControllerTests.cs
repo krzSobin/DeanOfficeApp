ba2fc7c62805cdc8536a5aa0e35bea3f9b840377
wypychasia <wypychasia@gmail.com>
2017-12-04 18:43:26 +0100

    testy init

---
+++ DeanOfficeApp.Api/DeanOfficeApp.ApiTests/Controllers/StudentsControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DeanOfficeApp.Api.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeanOfficeApp.Api.DAL;
+using Moq;
+using DeanOfficeApp.Api.Models;
+using DeanOfficeApp.ApiTests;
+using DeanOfficeApp.Contracts;
+
+namespace DeanOfficeApp.Api.Controllers.Tests
+{
+    [TestClass()]
+    public class StudentsControllerTests
+    {
+        [TestInitialize]
+        public void ini()
+        {
+
+        }
+
+        //[TestMethod()]
+        //public void GetStudentsTest()
+        //{
+        //    var mock=MockCreator.GetMockStudents();
+        //    Assert.Fail();
+        //}
+
+        [TestMethod()]
+        public async Task PostStudentAsyncTest()
+        {
+            var mock = MockCreator.GetMockStudents();
+            var loggingMock = MockCreator.getLoggingMock();
+            CreateStudentDTO student = new CreateStudentDTO();
+            student.Email = "a@a.pl";
+            student.FirstName = "Joanna";
+            student.LastName = "Wypych";
+            student.CurrentSemester = 1;
+            student.EnrollmentDate = DateTime.Now;
+            student.Pesel = 95062555224;
+
+            StudentsController controller = new StudentsController(mock.Object, loggingMock.Object);
+
+            await controller.PostStudentAsync(student);
+
+            var studentList = controller.GetStudents();
+            var isAdded=studentList.Any(stud=>stud.Pesel==student.Pesel);
+
+            Assert.IsTrue(isAdded);
+        }
+    }
+}
---
+++ DeanOfficeApp.Api/DeanOfficeApp.ApiTests/MockStudent.cs
@@ -0,0 +1,54 @@
+﻿using DeanOfficeApp.Api.DAL;
+using DeanOfficeApp.Api.DAL.Logging;
+using DeanOfficeApp.Api.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeanOfficeApp.ApiTests
+{
+    public class MockCreator
+    {
+        private static MockCreator _instance;
+        private MockCreator()
+        {
+            Student[] students = new Student[]
+            {
+                new Student{ RecordBookNumber = 271102, Pesel = 95062500222, CurrentSemester = 1, EnrollmentDate = DateTime.Now},
+                new Student{ RecordBookNumber = 271103, Pesel = 95062522222, CurrentSemester = 2, EnrollmentDate = DateTime.Now},
+                new Student{ RecordBookNumber = 271104, Pesel = 95062650222, CurrentSemester = 3, EnrollmentDate = DateTime.Now}
+            };
+
+            mock.Setup(m => m.GetStudents("")).Returns(students);
+        }
+        private Mock<IStudentRepository> mock = new Mock<IStudentRepository>();
+        private Mock<ILoggingRepository> loggingMock = new Mock<ILoggingRepository>();
+
+        public static Mock<IStudentRepository> GetMockStudents()
+        {
+            if (_instance == null)
+                _instance = new MockCreator();
+            return _instance.getMockStudent();
+
+        }
+        public static Mock<ILoggingRepository> getLoggingMock()
+        {
+            if (_instance == null)
+                _instance = new MockCreator();
+            return _instance.getMockLogging();
+        }
+
+        private Mock<IStudentRepository> getMockStudent()
+        {
+            return mock;
+        }
+
+        private Mock<ILoggingRepository> getMockLogging()
+        {
+            return loggingMock;
+        }
+    }
+}
